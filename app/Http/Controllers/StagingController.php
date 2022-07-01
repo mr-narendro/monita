92a2f29a65de11473eba74110c265216fe195dfb
mr-narendro <narendro.alif@gmail.com>
2022-07-01 08:55:31 +0700

    edit staging controller

--- app/Http/Controllers/StagingController.php
+++ app/Http/Controllers/StagingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use DataTables;
+use Exception;
 use App\events;
 
 
@@ -111,101 +112,118 @@ class StagingController extends Controller
     {
         $jenis = $request->jenis;
         $data = $request->data;
-        if ($jenis == 'bandwidth' || $jenis == 'produk') {
+        if ($jenis == 'produk' || $jenis == 'bandwidth') {
             $pli = DB::connection('dbo')->select(
-                DB::raw("SELECT spb.swo_ProjectActivatID as no_pa, ab.new_IDPEL as idpel, pli.swo_id AS PLI
+                DB::raw("SELECT spb.swo_ProjectActivatID as no_pa, ab.new_IDPEL as idpel, pli.swo_id AS PLI, CASE WHEN c.swo_id IS NULL
+                THEN '-' ELSE c.swo_id END AS bandwidth, pb.Name as produk
                 FROM swo_projectactivationBase spb
                 LEFT JOIN AccountBase ab on ab.AccountId = spb.swo_CustomerName
                 LEFT JOIN swo_productlineitemBase pli on pli.swo_ProductLineItemProjectActivatioId =
                 spb.swo_projectactivationId
-                where spb.swo_ProjectActivatID IN ('".$data."')")
+                LEFT JOIN swo_bandwidthBase c ON spb.swo_Bandwidth = c.swo_bandwidthId
+                LEFT JOIN ProductBase pb ON pb.ProductId = spb.swo_ProductNameId
+                where spb.swo_ProjectActivatID IN ('" . $data . "')")
             );
             // dd($pli);
-            foreach($pli as $p)
-            {
+            foreach ($pli as $p) {
 
                 $pli = $p->PLI;
                 $idpel = $p->idpel;
                 $pa = $p->no_pa;
-                if($pli == ''){
+                $produk = $p->produk;
+                $bandwidth = $p->bandwidth;
+                if ($pli == '') {
+
                     //cek opportunity
                     $opor = DB::connection('sqlsrv')->select(
-                        DB::raw("SELECT DISTINCT * FROM Tbl_Opportunity WHERE ID_PELANGGAN = '".$idpel."' OR CRMID =
-                        '".$idpel."'")
+                        DB::raw("SELECT * FROM Tbl_Opportunity WHERE ID_PELANGGAN = '" . $idpel . "' OR CRMID =
+                        '" . $idpel . "'")
                     );
                     // dd($opor);
-                    foreach($opor as $o){
+                    foreach ($opor as $o) {
                         $kode_produk_tv = $o->KODE_PRODUK_TV;
                         $harga_tv = $o->HARGA_PRODUK_TV;
                         $kode_produk_internet = $o->KODE_PRODUK_INTERNET;
                         $harga_internet = $o->HARGA_PRODUK_INTERNET;
                         // dd($kode_produk_tv, $harga_tv);
-                        $add_pli = DB::connection('sqlsrv')->statement(
-                            "EXEC SP_CRM_INSERTPLI @ID_PELANGGAN = '".$idpel."',
-                            @KODE_PRODUK_INTERNET = '".$kode_produk_internet."', @KODE_PRODUK_TV = '".$kode_produk_tv."',
-                            @HARGA_INTERNET = '".$harga_internet."', @HARGA_TV= '".$harga_tv."'"
+                        DB::connection('sqlsrv')->statement(
+                            "EXEC SP_CRM_INSERTPLI @ID_PELANGGAN = '" . $idpel . "',
+                            @KODE_PRODUK_INTERNET = '" . $kode_produk_internet . "', @KODE_PRODUK_TV = '" . $kode_produk_tv . "',
+                            @HARGA_INTERNET = '" . $harga_internet . "', @HARGA_TV= '" . $harga_tv . "'"
                         );
 
-                        if($add_pli){
-                            DB::connection('dbo')->update(DB::raw("UPDATE a7 SET
+
+                        $pl = DB::connection('dbo')->update(DB::raw("UPDATE a7 SET
                             a7.swo_ProductLineItemProjectActivatioId = c7.swo_ProjectActivationId FROM
                             swo_productlineitemBase a7
                             LEFT JOIN swo_ProjectActivationBase c7 ON c7.swo_serviceid = a7.new_serviceid
                             LEFT JOIN AccountBase b7 ON b7.AccountId = c7.swo_CustomerName
-                            WHERE b7.New_IDPEL = '".$idpel."'"));
+                            WHERE b7.New_IDPEL = '" . $idpel . "'"));
 
+                        //return redirect()->route('staging.index');
+                        if ($pl == TRUE) {
+                            if ($produk == '') {
+                                $updateProd = DB::connection('dbo')->update(
+                                    DB::raw("UPDATE a SET a.swo_ProductNameId = b.swo_product_findim FROM
+                                    swo_projectActivationBase a
+                                    LEFT JOIN swo_productlineitemBase b ON b.new_serviceid = a.swo_serviceid
+                                    LEFT JOIN swo_serviceidBase c ON c.swo_serviceidId = a.swo_serviceid
+                                    WHERE a.swo_ProjectActivatID = '" . $pa . "'")
+                                );
+                                if($updateProd == TRUE){
+                                    echo "<script>
+                                        alert('berhasil insert produk')
+                                    </script>";
+                                    return redirect()->route('staging.index');
+                                }else{
+                                    echo "ada kesalahan :(";
+                                }
 
-                            DB::connection('sqlsrv')->statement(
-                                "EXEC SP_UPDATE_PRODUCT_BW @_NOPA =
-                                '".$pa."'"
-                            );
+                            } elseif ($bandwidth == '') {
+                                $updateBW = DB::connection('sqlsrv')->statement("EXEC SP_UPDATE_PRODUCT_BW @_NOPA =
+                                '" . $pa . "'");
 
-                            echo "<script>
-                                alert('berhasil insert data')
-                            </script>";
+                                if($updateBW == TRUE){
+                                    echo "<script>
+                                        alert('berhasil insert bandwidth')
+                                    </script>";
+                                    return redirect()->route('staging.index');
+                                }else{
+                                    echo "ada kesalahan :(";
+                                }
 
-                            return redirect()->route('staging.index');
-                        }else{
-                            echo "gagal insert";
+                            }
                         }
                     }
-                }elseif($pli != ''){
-                    DB::connection('sqlsrv')->statement(
-                    "EXEC SP_UPDATE_PRODUCT_BW @_NOPA =
-                    '".$pa."'"
-                    );
-
-                    echo "<script>
-                        alert('berhasil insert data')
-                    </script>";
-
-                    return redirect()->route('staging.index');
+                } elseif ($pli != '') {
+                    echo "pli sudah ada coy silahkan kembali";
+                    // return redirect()->route('staging.index');
+                } elseif ($produk != '') {
+                    echo "PRODUK sudah ada coy silahkan kembali";
+                    // return redirect()->route('staging.index');
+                } elseif ($bandwidth != '') {
+                    echo "bandwidth sudah ada coy silahkan kembali";
+                    // return redirect()->route('staging.index');
                 }
             }
-
-
-
-
         } elseif ($jenis == 'no_io') {
             DB::connection('sqlsrv')->statement("EXEC SP_CRM_INSERTIO @ID_PELANGGAN = '" . $data . "'
             ");
 
-                echo "<script>
+            echo "<script>
                         alert('berhasil insert data io')
                       </script>";
 
-                return redirect()->route('staging.index');
-
+            return redirect()->route('staging.index');
         } elseif ($jenis == 'no_pi') {
             DB::connection('sqlsrv')->statement("EXEC SP_CRM_INSERTPI @ID_PELANGGAN = '" . $data . "'
             ");
 
-                echo "<script>
+            echo "<script>
                     alert('berhasil insert data pi')
                 </script>";
 
-                return redirect()->route('staging.index');
-
+            return redirect()->route('staging.index');
         }
     }
 }
