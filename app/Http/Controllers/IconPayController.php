0c03b427347649406111b5d02e4858cd9aeda93f
jokotingkir <robertto.rachmat@gmail.com>
2022-07-28 17:36:07 +0700

    edit env and save log

--- app/Http/Controllers/IconPayController.php
+++ app/Http/Controllers/IconPayController.php
@@ -425,29 +425,29 @@ class IconPayController extends Controller
         $KODEINVOICE = $request->kodeInvoice;
         $DATA = $request->data;
 
-        DB::connection('sarma_dev')->table('Tbl_LogTrxIconpay')->insert([
-            'CONTROLLER' => $CONTROLLER,
-            'JENISTRANSAKSI' => $JENISTRANSAKSI,
-            'KODERC' => $KODERC,
-            'KETERANGAN' => $KETERANGAN,
-            'IDPELANGGAN' => $IDPELANGGAN,
-            'PERIODETAGIHAN' => $PERIODETAGIHAN,
-            'CREATEDON' => date('Y-m-d h:m:s'),
-            'KODEINVOICE' => $KODEINVOICE,
-            'DATA' => $DATA,
-        ]);
-
-        // DB::connection('sqlsrv')->table('Tbl_LogTrxIconpay')->insert([
-        // 'CONTROLLER' => $CONTROLLER,
-        // 'JENISTRANSAKSI' => $JENISTRANSAKSI,
-        // 'KODERC' => $KODERC,
-        // 'KETERANGAN' => $KETERANGAN,
-        // 'IDPELANGGAN' => $IDPELANGGAN,
-        // 'PERIODETAGIHAN' => $PERIODETAGIHAN,
-        // 'CREATEDON' => date('Y-m-d h:m:s'),
-        // 'KODEINVOICE' => $KODEINVOICE,
-        // 'DATA' => $DATA,
-        // 'CREATEDBY' => ''
+        // DB::connection('sarma_dev')->table('Tbl_LogTrxIconpay')->insert([
+        //     'CONTROLLER' => $CONTROLLER,
+        //     'JENISTRANSAKSI' => $JENISTRANSAKSI,
+        //     'KODERC' => $KODERC,
+        //     'KETERANGAN' => $KETERANGAN,
+        //     'IDPELANGGAN' => $IDPELANGGAN,
+        //     'PERIODETAGIHAN' => $PERIODETAGIHAN,
+        //     'CREATEDON' => date('Y-m-d h:m:s'),
+        //     'KODEINVOICE' => $KODEINVOICE,
+        //     'DATA' => $DATA,
         // ]);
+
+        DB::connection('sqlsrv')->table('Tbl_LogTrxIconpay')->insert([
+        'CONTROLLER' => $CONTROLLER,
+        'JENISTRANSAKSI' => $JENISTRANSAKSI,
+        'KODERC' => $KODERC,
+        'KETERANGAN' => $KETERANGAN,
+        'IDPELANGGAN' => $IDPELANGGAN,
+        'PERIODETAGIHAN' => $PERIODETAGIHAN,
+        'CREATEDON' => date('Y-m-d h:m:s'),
+        'KODEINVOICE' => $KODEINVOICE,
+        'DATA' => $DATA,
+        'CREATEDBY' => 'IT PIKET'
+        ]);
     }
 }
